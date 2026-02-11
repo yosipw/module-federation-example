c53d3030e6b5f3ac4f874a8930d81133cad2fb60
yosipw <yossi@medirecords.com>
2026-02-11 19:28:33 +0800

    Downgrade storybook

--- libs/web-components/src/lib/register-elements.ts
+++ libs/web-components/src/lib/register-elements.ts
@@ -2,6 +2,8 @@ import { Card } from './card/card';
 import { Icon } from './icon/icon';
 
 export function registerElements(): void {
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
   customElements.get('wc-card') || customElements.define('wc-card', Card);
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
   customElements.get('wc-icon') || customElements.define('wc-icon', Icon);
 }
